4042f9d1f9d5eadaf3f693629ecbcd0713bc4063
Jack Parsons <jcksnparsons@yahoo.com>
2020-04-30 16:16:28 -0500

    clean up work and minor refactoring

--- instructor.py
+++ instructor.py
@@ -4,11 +4,6 @@ class Instructor(NSSPerson):
 
     def __init__(self, id, first, last, handle, cohort, specialty):
         super().__init__(id, first, last, handle, cohort)
-        # self.id = id
-        # self.first_name = first
-        # self.last_name = last
-        # self.slack_handle = handle
-        # self.cohort = cohort
         self.specialty = specialty
 
     def __repr__(self):
